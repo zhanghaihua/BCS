c3bb12caebcaf5c9c45decd52925e877aa7c6f1e
zhang haihua <zhanghaihua415@163.com>
2013-11-27 15:59:45 +0800

    Create create_object.js

---
+++ create_object.js
@@ -0,0 +1,31 @@
+var BCS = require('./bcs.js').BCS;
+var fs = require('fs');
+
+/*
+var hostname = 'bcs-sandbox.baidu.com';
+var ak = "EygRcI86pmJksQP9"; // 用户名
+var sk = "kgS7LjDm9h0lVLpKs7m25igsHO9WNy8tH4"; // 密码
+*/
+
+var hostname = 'bcs.duapp.com';
+var ak = 'yWS3boASGSBCgMwxGkF1ldNR';
+var sk = '4F1ww2vXOt1AC8OrRs35vuyTv88AgKZU';
+
+
+var options = {
+    'host': hostname,
+    'ak': ak,
+    'sk': sk
+}
+
+var client = new BCS(options);
+
+
+client.create_object('lighttest', '/bubugao.mp3', 'd:/download/bubugao.mp3', {'callback' : function(err, res){
+	if (err) {
+		console.log(res);
+	} else {
+		console.log( res ? "Object exist!" : "Object may not exist!");
+	}
+}
+});
